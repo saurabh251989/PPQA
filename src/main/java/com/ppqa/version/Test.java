ac7d7c2e12d33c347a28becfeca91931ee350472
saurabh <someonesachinkumar@gmail.com>
2016-08-14 14:36:33 +0530

    Working Code

--- src/main/java/com/ppqa/version/Test.java
+++ src/main/java/com/ppqa/version/Test.java
@@ -8,8 +8,8 @@ import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+
 import org.jsoup.nodes.Node;
-import org.jsoup.select.Elements;
 
 public class Test {
 
@@ -22,53 +22,38 @@ public class Test {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println("Sudip");
-		System.out.println("Sp");
 
-		System.out.println("Hello");
-		System.out.println("*****************");
 		List<Node> childList = doc.body().childNodes();
-		for (Iterator<Node> iterator = childList.iterator(); iterator.hasNext();) {
-			Node node = (Node) iterator.next();
-			// System.out.println(node.nodeName());
-		}
+
 		List<Node> childListNodde3 = childList.get(3).childNodes();
-		for (Iterator iterator = childListNodde3.iterator(); iterator.hasNext();) {
+		for (Iterator<Node> iterator = childListNodde3.iterator(); iterator.hasNext();) {
 			Node node = (Node) iterator.next();
-			// System.out.println(node.nodeName());
-			 System.out.println("Hello");
+
 			List<Node> childNode = node.childNodes();
-			List<Node> childNode1=new ArrayList<Node>();
-			 System.out.println(childNode.get(0));
 
-			for (Iterator<Node> iterator2 = childNode.iterator(); iterator2.hasNext();) {
-				Node node1 = (Node) iterator2.next();
-				if(node1.childNodeSize()==0 && node1.nodeName().equals("#text") )
-					{
-					//childNode.remove(node1);
-					continue;
-					}
-				childNode1.add(node1);
-      			// System.out.println( node1.nodeName());
+			System.out.println(childNode.get(10).childNode(0).childNode(0).childNode(0));
 
-			}
+			System.out.println(childNode.get(10).childNode(12).childNode(0));
+			System.out.println(childNode.get(10).childNode(14).childNode(0));
 
-
-			for (Iterator<Node> iterator2 = childNode1.iterator(); iterator2.hasNext();) {
-				Node node2 = (Node) iterator2.next();
-				/// System.out.println( node2.nodeName()+"\t"+node2.childNodeSize());
-			}
-
-			List<Node> xyz = childNode.get(0).childNodes();
-for (Iterator iterator2 = xyz.iterator(); iterator2.hasNext();) {
-	Node node2 = (Node) iterator2.next();
-	 System.out.println( node2.nodeName()+"\t"+node2.childNodeSize());
-
-}
 		}
 
-		Elements element = doc.body().getElementsByTag("tbody");
-		// System.out.println(element);
+	}
+
+	static List<Node> removeTextNode(List<Node> childNode) {
+
+		List<Node> nodeList = new ArrayList<Node>();
+
+		for (Iterator<Node> iterator2 = childNode.iterator(); iterator2.hasNext();) {
+			Node node1 = (Node) iterator2.next();
+			if (node1.childNodeSize() == 0 && node1.nodeName().equals("#text")) {
+				// childNode.remove(node1);
+				continue;
+			}
+			nodeList.add(node1);
+
+		}
+		return nodeList;
 
 	}
 
